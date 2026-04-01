dd96967c4611a84a9305dff97cb2643be83fd9e7
Krishnasanjay <Krishnasanjay1012@gmail.com>
2026-04-01 15:03:19 +0530

    added playwright dependency

--- tests/NDE_Admin/login.spec.js
+++ tests/NDE_Admin/login.spec.js
@@ -1,24 +1,12 @@
 
-// const { expect } = require("@playwright/test")
+const { expect } = require("@playwright/test")
 
-// async function login(page) {
-//   await page.goto("https://adminv2.nowdigitaleasy.com/login")
-//   await page.locator('[name="userName"]').fill("iaaxin")
-//   await page.locator('[name="password"]').fill("King_Guna")
-//   await page.getByRole("button",{name:"Login"}).click()
-//   await expect(page).toHaveURL(/home/)
-// }
+async function login(page) {
+  await page.goto("https://adminv2.nowdigitaleasy.com/login")
+  await page.locator('[name="userName"]').fill("iaaxin")
+  await page.locator('[name="password"]').fill("King_Guna")
+  await page.getByRole("button",{name:"Login"}).click()
+  await expect(page).toHaveURL(/home/)
+}
 
-// module.exports = { login }
-const {test,expect}=require("@playwright/test")
-
-test("practice",async ({ page })=>{
-   await page.goto("https://practicetestautomation.com/practice-test-login/")
-await page.locator('[name="username"]').fill("student")
-await page.locator('[name="password"]').fill("Password123")
-await page.getByRole("button",{name:"Submit"}).click()
-await expect(page).toHaveURL("https://practicetestautomation.com/logged-in-successfully/")
-await page.getByRole("link",{name:"Log out"}).click()
-await expect(page).toHaveURL("https://practicetestautomation.com/practice-test-login/")
-
-})
+module.exports = { login }
